ef2de19655894e9102119b420e86212311852684
Rohan Mehrotra <rohan.mehrotra@octro.com>
2018-03-09 18:25:23 +0530

    Player movement fixed
    From rate is very low

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,11 +25,13 @@ public class PlayerController : MonoBehaviour
 
 
 	[SerializeField]
-	private float thrusterPower = 3.0f;
+	private float thrusterPower = 1.0f;
 
+
+	private Ray ray;
 
 	private PlayerMotor motor;
-
+	private RaycastHit hit;
 
 	private void Awake()
 	{
@@ -41,8 +43,9 @@ public class PlayerController : MonoBehaviour
     {
         motor = GetComponent<PlayerMotor>();
 		joint = GetComponent<ConfigurableJoint> ();
-
-	//	setJointSettings (jointSpring);
+		ray.origin = gameObject.transform.position;
+		ray.direction = Vector3.down;
+		setJointSettings (jointSpring);
     }
 
 
@@ -61,15 +64,23 @@ public class PlayerController : MonoBehaviour
         motor.Move(_velocity);
       
         float _yRot = Input.GetAxisRaw("Mouse X");
-
-		Vector3 _rotation = new Vector3(0,_yRot,0)*horizontalLookSensitivity;
+		float yRotation = _yRot * horizontalLookSensitivity;
+	
+		Vector3 _rotation = new Vector3(0,yRotation,0);
         motor.Rotate(_rotation);
 
 
 
         float _xRot = Input.GetAxisRaw("Mouse Y");
+		float _xRotation = _xRot * verticalLookSensitivity;
 
-		Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) *verticalLookSensitivity;
+		if (_xRotation >= 85f) {
+			_xRotation = 85f;
+		}
+		if (_xRotation <= -85f) {
+			_xRotation = 85f;
+		}
+		Vector3 _cameraRotation = new Vector3(_xRotation, 0, 0) ;
 
         motor.CameraRotate(_cameraRotation);
 
@@ -92,11 +103,43 @@ public class PlayerController : MonoBehaviour
 		} else {
 			gameObject.GetComponent<ConfigurableJoint> ().yDrive = new JointDrive {positionSpring=jointSpring,maximumForce=1000};
 		}*/
-		if (Input.GetKey (KeyCode.Space)) 
+		ray.origin = gameObject.transform.position;
+		ray.direction = Vector3.down;
+		if (Physics.Raycast (ray, out hit,100f)) 
 		{
+			joint.targetPosition = new Vector3 (0, -(hit.point.y)+0.5f, 0);
+
+			Debug.Log (hit.collider.name);
+			Debug.DrawRay (gameObject.transform.position,Vector3.down);
+		}
+		else 
+		{
+			joint.targetPosition = Vector3.zero;
+		}
+		if (Input.GetKey (KeyCode.Space) && thrusterPower>=0) 
+		{
+			
 			_thrusterForce = Vector3.up * thrusterForce;
+			gameObject.GetComponent<ConfigurableJoint> ().yDrive = new JointDrive{ 
+				positionSpring=0,
+				maximumForce=jointMaxForce
+			};
+			thrusterPower -= 2*Time.deltaTime;	
+			//setJointSettings (0);
 
-
+		}
+		else 
+		{	
+			thrusterPower += Time.deltaTime;
+			if (thrusterPower >= 1f) {
+				thrusterPower = 1f;
+			}
+			_thrusterForce = Vector3.zero;
+			gameObject.GetComponent<ConfigurableJoint> ().yDrive = new JointDrive {
+				positionSpring = jointSpring,
+				maximumForce = jointMaxForce
+			};
+			setJointSettings (jointSpring);
 		}
 
 
@@ -105,7 +148,9 @@ public class PlayerController : MonoBehaviour
 
 	private void setJointSettings(float _jointSpring)
 	{
-		joint.yDrive = new JointDrive {
+		joint.yDrive = new JointDrive 
+			{
+			
 			positionSpring=jointSpring,
 			maximumForce=jointMaxForce
 		};
